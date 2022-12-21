ddf0e3a1a69ec5324aac338dd0e66cc57fb51c6d
Thiago Ferreira <thiagoferreira@NATURAL-012839.local>
2022-12-21 08:56:58 -0300

    Including new exercises about arrays in java

--- java/src/arrays/Exercise07.java
+++ java/src/arrays/Exercise07.java
@@ -1,4 +1,22 @@
 package arrays;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Exercise07 {
+    /*
+     * 7 - Write a Java program to remove a specific element from an array.
+     */
+    public static void main(String[] args) {
+        int[] myArray = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        int removeIndex = 1;
+
+        System.out.println("Original Array: " + Arrays.toString(myArray));
+
+        for (int i = removeIndex; i < myArray.length - 1; i++) {
+            myArray[i] = myArray[i + 1];
+        }
+
+        System.out.println("After removing the second element: " + Arrays.toString(myArray));
+    }
 }
